e53d1abaa21141c1f1370197cca28d946822df66
Pedro Fontanarrosa <pfontanarrosa@gmail.com>
2023-07-14 15:14:45 +0100

    Added new test file (data.csv) and two python files (DataSimulate.py and GPFit.py) to the repository.

---
+++ DataSimulate.py
@@ -0,0 +1,59 @@
+# This script will be used to simulate the data that will be used to fit the Gaussian Process
+
+# Import the required libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+# Define the function that will simulate the data
+# This function is the Lotka-Volterra predator-prey model
+# It takes two arguments: t, the time, and y, a vector of the current population sizes
+# It returns a list of the time derivatives of the populations, in the same order as the input
+
+def lotka_volterra(t, y):
+    mu = [0.2, 0.7, 0.9]
+    M = np.array([[-0.1, 0.0, 0.0], [0.0, -0.1, 0.1], [0.1, 0.0, -0.1]])
+
+    y1 = y[0]
+    y2 = y[1]
+    y3 = y[2]
+
+    dy1 = y1*mu[0] + y1*(M[0, 0]*y1 + M[0, 1]*y2 + M[0, 2]*y3)
+    dy2 = y2*mu[1] + y2*(M[1, 0]*y1 + M[1, 1]*y2 + M[1, 2]*y3)
+    dy3 = y3*mu[2] + y3*(M[2, 0]*y1 + M[2, 1]*y2 + M[2, 2]*y3)
+
+    return [dy1, dy2, dy3]
+
+
+def simulate(y0, t):
+    return solve_ivp(fun=lotka_volterra, t_span=[min(t), max(t)], y0=y0, t_eval=t, method='LSODA')
+
+
+# Define the initial conditions
+nps = 31
+t = np.linspace(0, 25, nps)
+y0 = [10.0, 10.0, 10.0]
+sol = simulate(y0, t)
+
+# sample data points
+# s_idx = np.random.choice(len(t), size = 101, replace=False)
+# s_idx.sort()
+s_idx = np.arange(nps)
+ts = sol.t[s_idx]
+ys = sol.y[:, s_idx]
+
+# add noise to growth data
+y_hat = np.maximum(ys + np.random.normal(scale=0.01, size=ys.shape), 0)
+
+# plot the data
+fig, ax = plt.subplots(figsize=(15, 5), ncols=3, nrows=1)
+ax[0].plot(ts, y_hat[0, :], "bx", mew=2)
+ax[1].plot(ts, y_hat[1, :], "gx", mew=2)
+ax[2].plot(ts, y_hat[2, :], "rx", mew=2)
+
+# show the plot
+plt.show()
+
+# export the data as a csv file
+np.savetxt("data.csv", np.vstack((ts, y_hat)).T, delimiter=",", header="t,y1,y2,y3", comments="")
+
---
+++ GPFit.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 14:51:00 2019
+Author: Pedro Fontanarrosa
+"""
+
+
+
+# This script will fit a Gaussian Process to the imported data
+# and then plot the results
+# To do so, it will first import the data from the file
+# and then generate a library of all the possible kernel, mean, and latent processes combinations
+# It will then fit each of these combinations to the data
+# then it will report the best fitted model
+# and finally it will plot the results of the best fitted model
+# The script will also save the results of the best fitted model to a file
+# The script could also plot all the BIC values for each of the fitted models and the one chosen to be the best
+# The script could also plot the results of the best fitted model with the data
+# The script could also plot the results of the best fitted model with the data and the 95% confidence interval
+# This script will be able to be run from the command line with the following arguments:
+# -i <input file> -o <output file path> 
+
+
+
+
+# Import the necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import gpflow as gpf # This is the library that will be used to fit the Gaussian Process
+import pandas as pd
+import os
+import sys
+import time
+import datetime
+import pickle
+
+# Store the input arguments
+
+# Check if the input file exists, if not, exit the script with an error message
+inputFile = sys.argv[1]
+if not os.path.isfile(inputFile):
+    print('The input file does not exist')
+    sys.exit()
+# Check if the input file is empty, if it is, exit the script with an error message
+if os.path.getsize(inputFile) == 0:
+    print('The input file is empty')
+    sys.exit()
+
+# Check if the input file is a csv file, if not, exit the script with an error message
+if not os.path.splitext(inputFile)[1] == '.csv':
+    print('The input file is not a csv file')
+    sys.exit()
+
+# Check if the output file path exists, if not, set it to the current directory
+if len(sys.argv) > 2:
+    outputFile = sys.argv[2]
+    if not os.path.isdir(os.path.dirname(outputFile)):
+        print('The output file path does not exist, setting it to the current directory')
+        outputFile = os.path.join(os.getcwd(), os.path.basename(outputFile))
+
+
+# Define the function that will import the data from the file
+def importData(inputFile):
+    # Import the data from the file
+    data = pd.read_csv(inputFile, sep=',', header=None)
+    # Return the data
+    return data
+
+
+# Define the function that will augment the data for the Gaussian Process
+def augmentData(timeSeries, y):
+    # Augment the input with ones or zeros to indicate the required output dimension
+    X_aug = np.vstack(
+        (np.hstack((timeSeries, np.zeros_like(timeSeries))),
+         np.hstack((timeSeries, np.ones_like(timeSeries))),
+         np.hstack((timeSeries, 2*np.ones_like(timeSeries)))
+         )
+    )
+    # Augment the Y data with ones or zeros that specify a likelihood from the list of likelihoods
+    Y1 = y[:, 0].reshape(-1, 1)
+    Y2 = y[:, 1].reshape(-1, 1)
+    Y3 = y[:, 2].reshape(-1, 1)
+
+    Y_aug = np.vstack(
+        (np.hstack((Y1, np.zeros_like(Y1))),
+         np.hstack((Y2, np.ones_like(Y2))),
+         np.hstack((Y3, 2*np.ones_like(Y3)))
+         )
+    )
+    # Return the augmented data
+    return X_aug, Y_aug
+
+# Define the function that will generate the library of all the possible kernel, mean, and latent processes combinations
+def generateLibrary():
+    # Define the list of all the possible kernel, mean, and latent processes
+    kernelList = [gpf.kernels.SquaredExponential, gpf.kernels.Matern32, gpf.kernels.RationalQuadratic, gpf.kernels.Exponential, gpf.kernels.Linear,
+           gpf.kernels.Cosine, gpf.kernels.Periodic, gpf.kernels.Polynomial, gpf.kernels.Matern12, gpf.kernels.Matern52, gpf.kernels.White]
+    meanList = [gpf.mean_functions.Constant(), gpf.mean_functions.Linear(), gpf.mean_functions.Identity(), gpf.mean_functions.Zero(), 
+                gpf.mean_functions.Polynomial(2), gpf.mean_functions.Polynomial(3), gpf.mean_functions.Polynomial(4), gpf.mean_functions.Polynomial(5)]
+    latentList = [gpf.likelihoods.Gaussian(), gpf.likelihoods.StudentT()]
+    # Define the list of all the possible combinations of kernel, mean, and latent processes
+    library = []
+    for kernel in kernelList:
+        for mean in meanList:
+            for latent in latentList:
+                library.append([kernel, mean, latent])
+    # Return the library
+    return library
+
+
+
+# From the imported data, extract the time series and the y values
+data = importData(inputFile)
+timeSeries = data.iloc[:, 0].values
+y = data.iloc[:, 1:].values
+X, Y = augmentData(timeSeries, y)
+
+X = timeSeries.reshape(-1, 1)
+Y = y.T
+
+# Augment the input with ones or zeros to indicate the required output dimension
+X_aug = np.vstack(
+    (np.hstack((X, np.zeros_like(X))),
+     np.hstack((X, np.ones_like(X))),
+     np.hstack((X, 2*np.ones_like(X)))
+     )
+)
+
+# Augment the Y data with ones or zeros that specify a likelihood from the list of likelihoods
+Y1 = Y[:, 0].reshape(-1, 1)
+Y2 = Y[:, 1].reshape(-1, 1)
+Y3 = Y[:, 2].reshape(-1, 1)
+
+Y_aug = np.vstack(
+    (np.hstack((Y1, np.zeros_like(Y1))),
+     np.hstack((Y2, np.ones_like(Y2))),
+     np.hstack((Y3, 2*np.ones_like(Y3)))
+     )
+)
+
+print(X_aug.shape)
+print(Y_aug.shape)
+
+
+
+
