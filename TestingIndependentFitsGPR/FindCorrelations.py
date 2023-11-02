0928e02349dcf44086eb53103e138b217b15e01f
Pedro Fontanarrosa <pfontanarrosa@gmail.com>
2023-11-02 10:40:17 +0000

    just linting issues fix

--- TestingIndependentFitsGPR/FindCorrelations.py
+++ TestingIndependentFitsGPR/FindCorrelations.py
@@ -1,18 +1,19 @@
-# This script finds the correlations between samples of three different species. 
+# This script finds the correlations between samples of three different species.
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import scipy.stats as stats
-import os
-import sys
-import math
+
 
 # Import the data mu_1.csv, mu_2.csv, and mu_3.csv and combine them into one data frame
-data1 = pd.read_csv(r"C:\Users\User\Dropbox\UCL\GPs\TestingIndependentFitsGPR\Testoutput\outputs_GPR\simulations0\Y_1mu1.csv")
-data2 = pd.read_csv(r"C:\Users\User\Dropbox\UCL\GPs\TestingIndependentFitsGPR\Testoutput\outputs_GPR\simulations0\Y_2mu1.csv")
-data3 = pd.read_csv(r"C:\Users\User\Dropbox\UCL\GPs\TestingIndependentFitsGPR\Testoutput\outputs_GPR\simulations0\Y_3mu1.csv")
+data1 = pd.read_csv(
+    r"C:\Users\User\Dropbox\UCL\GPs\TestingIndependentFitsGPR\Testoutput\outputs_GPR\simulations0\Y_1mu1.csv")
+data2 = pd.read_csv(
+    r"C:\Users\User\Dropbox\UCL\GPs\TestingIndependentFitsGPR\Testoutput\outputs_GPR\simulations0\Y_2mu1.csv")
+data3 = pd.read_csv(
+    r"C:\Users\User\Dropbox\UCL\GPs\TestingIndependentFitsGPR\Testoutput\outputs_GPR\simulations0\Y_3mu1.csv")
 data = pd.concat([data1, data2, data3], axis=1)
 data.columns = ["Y_1", "Y_2", "Y_3"]
 
@@ -24,7 +25,8 @@ correlations = data.corr()
 print(correlations)
 
 # Plot the correlations
-sns.heatmap(correlations, xticklabels=correlations.columns, yticklabels=correlations.columns, cmap="YlGnBu")
+sns.heatmap(correlations, xticklabels=correlations.columns,
+            yticklabels=correlations.columns, cmap="YlGnBu")
 plt.title("Correlations between species")
 plt.show()
 
@@ -41,17 +43,16 @@ plt.show()
 # The alternative hypothesis is that the samples are not independent
 
 # Find the p-values for the correlations
-p_values = np.zeros((3,3))
+p_values = np.zeros((3, 3))
 for i in range(3):
     for j in range(3):
-        p_values[i,j] = stats.pearsonr(data.iloc[:,i], data.iloc[:,j])[1]
+        p_values[i, j] = stats.pearsonr(data.iloc[:, i], data.iloc[:, j])[1]
 
 # Print the p-values
 print(p_values)
 
 # Plot the p-values
-sns.heatmap(p_values, xticklabels=correlations.columns, yticklabels=correlations.columns, cmap="YlGnBu")
+sns.heatmap(p_values, xticklabels=correlations.columns,
+            yticklabels=correlations.columns, cmap="YlGnBu")
 plt.title("P-values for correlations between species")
 plt.show()
-
-
